277af6e2bb98c9476379f27b6021378dfd2953ed
Karimullin Arthur <KarimullinArthur@riseup.net>
2024-03-03 00:35:08 +0300

    Start game in lobby with K_SPACE

--- spaceway/scenes/lobby/functions.py
+++ spaceway/scenes/lobby/functions.py
@@ -2,11 +2,16 @@ from sys import exit
 
 import pygame
 
+from .objects import PlayButton
+
 
 def check_events(config, scene_buttons):
     for event in pygame.event.get():
         if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
             exit()
+
+        elif event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+            scene_buttons.get_by_instance(PlayButton).press()
 
         elif event.type == pygame.MOUSEBUTTONDOWN:
             x, y = pygame.mouse.get_pos()
@@ -20,7 +25,11 @@ def check_events(config, scene_buttons):
                 config.save()
 
 
-def update(bg, scene_buttons, caption):
+def update(bg, scene_buttons, caption, config):
+    pygame.mixer.unpause()
+    if not config['ns'].mm.get('lobby').get_num_channels():
+        config['ns'].mm.get('lobby').play(-1)
+
     bg.blit()
 
     scene_buttons.draw()
